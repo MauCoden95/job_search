c1960f60b3c763e25bd328250efac2ee117d7d93
Your Name <you@example.com>
2023-12-18 17:50:02 -0300

    Primera parte vista login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,15 +15,15 @@
       }
     }
   </script>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <title>Document</title>
+ <script src="https://kit.fontawesome.com/7483adbd94.js" crossorigin="anonymous"></script>
+    <title>Job Search - Login</title>
 </head>
 <body>
     <header class="w-full h-24 bg-blue-950">
         <div class="w-5/6 h-full m-auto text-white flex items-center justify-between">
-                <a href="/" className="text-xl" style="font-size: 1.7rem;">
+                <a href="http://localhost:5173/" className="text-xl" style="font-size: 1.7rem;">
                     Job Search
-                    <i className="fas fa-briefcase ml-2"></i>
+                    <i class="fas fa-briefcase"></i>
                 </a>
 
                 <span className="text-2xl" style="font-size: 1.7rem;">
@@ -31,5 +31,62 @@
                 </span>
         </div>
     </header>
+
+    <section class="w-2/5 h-16 p-1 bg-gray-300 m-auto my-10 rounded-full flex">
+        <button class="w-2/4 h-full hover:bg-gray-500 transition cursor-pointer rounded-s-full">Login candidato</button>
+        <button class="w-2/4 h-full hover:bg-gray-500 transition cursor-pointer rounded-e-full">Login empresa</button>
+    </section>
+
+    <section class="w-full min-h-0">
+        <div class="w-5/6 h-full m-auto py-5 text-white flex items-center justify-evenly">
+              <img class="w-2/5" src="/img/programmer.png" alt="Programador">
+              <form class="w-2/5" method="POST" action="{{ route('login') }}">
+                @csrf
+
+                <!-- Email Address -->
+                <div>
+                    <x-input-label class="text-lg" for="email" :value="__('Correo Electrónico')" />
+                    <x-text-input id="email" class="block border-2 border-black mt-1 w-full p-2 text-black" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <!-- Password -->
+                <div class="mt-4">
+                    <x-input-label class="text-lg" for="password" :value="__('Contraseña')" />
+
+                    <x-text-input id="password" class="block border-2 border-black mt-1 w-full p-2 text-black"
+                                    type="password"
+                                    name="password"
+                                    required autocomplete="current-password" />
+
+                    <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                </div>
+
+               
+
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                            {{ __('Olvidó su contraseña?') }}
+                        </a>
+                    @endif
+
+                    <x-primary-button class="ms-3">
+                        {{ __('Ingresar') }}
+                    </x-primary-button>
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <section class="w-2/6 min-h-0 py-2 m-auto my-10 flex items-center justify-evenly">
+        <a href="#"><i class="fab fa-facebook text-4xl hover:text-blue-800"></i></a>
+        <a href="#"><i class="fab fa-linkedin text-4xl hover:text-blue-500"></i></a>
+        <a href="#"><i class="fab fa-twitter text-4xl hover:text-blue-300"></i></a>
+        <a href="#"><i class="fab fa-instagram-square text-4xl hover:text-violet-500"></i></a>
+        <a href="#"><i class="fab fa-youtube text-4xl hover:text-red-600"></i></a>
+    </section>
+
+    @include('layouts.footer')
 </body>
 </html>
